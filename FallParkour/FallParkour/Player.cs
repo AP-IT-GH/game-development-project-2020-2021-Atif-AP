61e442a86bd0f54b6f4fd7f94929159c72b5e009
Atif <s107214@ap.be>
2020-12-23 22:43:53 +0100

    Fixed player Animation and Added Gravity (work in progress)

--- FallParkour/FallParkour/Player.cs
+++ FallParkour/FallParkour/Player.cs
@@ -13,43 +13,75 @@ namespace FallParkour
     {
         Texture2D heroTexture;
         Animatie animatie;
-        Vector2 position;
+        Vector2 position, velocity;
+        const float gravity = 100f;
+        float jumpSpeed = 500f;
+        float moveSpeed = 500f;
+        bool jump = true;
 
         public Player(Texture2D texture)
         {
             heroTexture = texture;
             animatie = new Animatie();
             animatie.AddFrame(new AnimationFrame(new Rectangle(0, 0, 32, 32)));
+            animatie.AddFrame(new AnimationFrame(new Rectangle(32, 0, 32, 32)));
             animatie.AddFrame(new AnimationFrame(new Rectangle(64, 0, 32, 32)));
             animatie.AddFrame(new AnimationFrame(new Rectangle(96, 0, 32, 32)));
             animatie.AddFrame(new AnimationFrame(new Rectangle(128, 0, 32, 32)));
-            animatie.AddFrame(new AnimationFrame(new Rectangle(140, 0, 32, 32)));
-            animatie.AddFrame(new AnimationFrame(new Rectangle(162, 0, 32, 32)));
+            animatie.AddFrame(new AnimationFrame(new Rectangle(160, 0, 32, 32)));
+        }
+
+        public void Initialize()
+        {
+            position = velocity = Vector2.Zero;
         }
 
         public void Update()
         {
             KeyboardState state = Keyboard.GetState();
+            GameTime gameTime = new GameTime();
 
             if(state.IsKeyDown(Keys.Right))
             {
-                position.X += 10;
+                velocity.X += moveSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
+                animatie.Update();
             }
             if (state.IsKeyDown(Keys.Left))
             {
-                position.X -= 10;
+                velocity.X -= moveSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
+                animatie.Update();
             }
-            if (state.IsKeyDown(Keys.Up))
+            if (state.IsKeyDown(Keys.Up) && jump)
             {
-                position.X += 10;
+                velocity.Y = -jumpSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
+                jump = false;
             }
 
-            animatie.Update();
+            if (!jump)
+            {
+                velocity.Y += gravity * (float)gameTime.ElapsedGameTime.TotalSeconds;
+            }
+            else
+            {
+                velocity.Y = 0;
+            }
+
+            velocity.Y += gravity * (float)gameTime.ElapsedGameTime.TotalSeconds;
+            position += velocity;
+
+            position.X += velocity.X;
+
+            jump = position.Y >= 150;
+
+            if (jump)
+            {
+                position.Y = 150;
+            }
         }
 
         public void Draw(SpriteBatch spriteBatch)
         {
-            spriteBatch.Draw(heroTexture, new Vector2(10, 10), animatie.CurrentFrame.SourceRectangle, Color.White);
+            spriteBatch.Draw(heroTexture, position, animatie.CurrentFrame.SourceRectangle, Color.White);
         }
     }
 }
